6eb1b6f65d36193b064b7706c6264652160971d8
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-09-13 21:07:17 +0400

    Data types and variables

--- BCMFH-20-NC-2/Lecture2/Program.cs
+++ BCMFH-20-NC-2/Lecture2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Nika");
+            //არითმეტიკული ოპერატორები + - * / %
+
+            string name = "Ilia";
+            string lastName = "Metreveli";
+
+            ////IMPLICIT ტიპიზიცაია
+            //byte y = 5;
+            //int x = y;
+
+
+            //EXPLICIT ტიპიზიცაია
+            //int y = 600;
+            //byte x = checked((byte)y);
+            //Console.WriteLine($"X: {x} Y: {y}");
+
+
+            //Console.WriteLine("Hello" + " " + name);
+            //Console.WriteLine($"Hello {name} {lastName}");
+            //Console.WriteLine("Hello {1} {0}", name, lastName);
+
+
+
+
+
         }
     }
 }
