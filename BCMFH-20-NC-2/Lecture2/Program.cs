cf1f0cd249a105681af9e63b5ee93c527093f157
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-09-13 22:27:42 +0400

    .

--- BCMFH-20-NC-2/Lecture2/Program.cs
+++ BCMFH-20-NC-2/Lecture2/Program.cs
@@ -5,9 +5,17 @@
         static void Main(string[] args)
         {
             //არითმეტიკული ოპერატორები + - * / %
+            //int x = 1;
+            //int y = 2;
 
-            string name = "Ilia";
-            string lastName = "Metreveli";
+            // ++ -- += -= *= /=
+
+
+            //Console.WriteLine(x+y);
+
+
+            //string name = "Ilia";
+            //string lastName = "Metreveli";
 
             ////IMPLICIT ტიპიზიცაია
             //byte y = 5;
@@ -24,6 +32,52 @@
             //Console.WriteLine($"Hello {name} {lastName}");
             //Console.WriteLine("Hello {1} {0}", name, lastName);
 
+
+            //ლოგიკური ოპერატორები
+            // > < >= <= == !=
+
+            //Console.WriteLine(5 > 8); // false
+            //Console.WriteLine(18 >= 8); // True
+            //Console.WriteLine(1 <= 8); // True
+            //Console.WriteLine(8 == 8); //True
+            //Console.WriteLine(8 != 8);// False
+            //Console.WriteLine(!true);
+
+            //IF SWITCH
+
+            //int x = 20;
+            //int y = 10;
+
+            //if (x > y)
+            //{
+            //    x *= 2;
+            //    Console.WriteLine(x);
+            //}
+            //else if (x == y)
+            //{
+            //    Console.WriteLine(x + y);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(y);
+            //}
+
+
+            //1. მომხმარებელს სთხოვეთ შემოიყვანოს სახლი გვარი ასაკი
+            // გამარჯობა მე მქვია {სახელი} {გვარი} ვარ {ასაკი} წლის
+
+            //Console.Write("FIRSTNAME: ");
+            //string firstName = Console.ReadLine();
+
+            //Console.Write("LASTNAME: ");
+            //string lastName = Console.ReadLine();
+
+            //Console.Write("AGE: ");
+            //int age = int.Parse(Console.ReadLine());
+            ////int age = Convert.ToInt32(Console.ReadLine());
+
+
+            //Console.WriteLine($"Hello my name is {firstName} {lastName}. I am {age} years old");
 
 
 
