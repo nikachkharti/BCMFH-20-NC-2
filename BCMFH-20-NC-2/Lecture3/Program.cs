4ef4dd69b6473688666d1c8bb95bb5bbb452baea
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-09-16 21:49:21 +0400

    Loops and exceptions

---
+++ BCMFH-20-NC-2/Lecture3/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lecture3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //int x = 256;
+            //byte y = (byte)x;
+            //OverflowException
+
+            //int x = int.Parse("ASDF");
+            //FormatException
+
+
+            //try
+            //{
+            //    int x = 10;
+            //    Console.WriteLine(x / 0);
+            //}
+            //catch (Exception exception)
+            //{
+            //    Console.WriteLine(exception.Message);
+            //}
+
+            //DivideByZeroException
+
+            //try
+            //{
+            //    Console.Write("FirstName: ");
+            //    string firstName = Console.ReadLine();
+
+            //    if (firstName.Length == 1)
+            //    {
+            //        throw new ValidationException("Invalid name");
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+
+
+
+            //while
+
+            //int i = 0;
+            //while (i < 10)
+            //{
+            //    Console.WriteLine("GIORGI");
+            //    i++;
+            //}
+
+
+            //do while
+
+            //int i = 0;
+            //do
+            //{
+            //    Console.WriteLine("GIORGI");
+            //    i++;
+            //}
+            //while (i < 3);
+
+
+
+            //int i = 0;  საწყისი წერტილი
+            //while (i < 10) პირობა
+            //{
+            //    Console.WriteLine("GIORGI");
+            //    i++; რა მოუვიდეს საწყის მნიშვნელობას თუ პირობა დაკმაყოფილდა
+            //}
+
+            //for
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        if (i == 3)
+            //        {
+            //            continue;
+            //        }
+
+            //        Console.WriteLine($"i: {i}");
+            //    }
+            //}
+
+
+
+            //foreach
+
+        }
+    }
+}
