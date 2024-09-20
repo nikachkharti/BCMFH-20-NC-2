86aaeb6aeb2660cc15093716b64662cbba714f06
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-09-20 21:52:38 +0400

    Array

---
+++ BCMFH-20-NC-2/Lecture4/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Lecture4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //მასივი
+
+            //int[] array = new int[3];
+            //array[0] = 11;
+            //array[1] = 4;
+            //array[2] = 120;
+
+
+            //int[] array = [11, 4, 120];
+
+            //Random random = new Random();
+
+            //int[] array = new int[10];
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = random.Next();
+            //}
+
+
+
+            //int[][] secondCollection = new int[3][];
+            //secondCollection[0] = [10, 67, 3];
+            //secondCollection[1] = [1, 2, 3];
+            //secondCollection[2] = [10, 6, 3];
+
+            //ROW 1 : 
+            //ROW 2 : 
+            //ROW 3 : 
+
+            //int result = 0;
+
+            //for (int i = 0; i < secondCollection.Length; i++)
+            //{
+            //    result += secondCollection[i][i];
+            //}
+
+            //Console.WriteLine(result);
+
+
+
+            //for (int i = 0; i < secondCollection.Length; i++)
+            //{
+            //    int max = secondCollection[i][0];
+
+            //    for (int j = 0; j < secondCollection[i].Length; j++)
+            //    {
+            //        if (secondCollection[i][j] > max)
+            //        {
+            //            max = secondCollection[i][j];
+            //        }
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+
+            int i = 13;
+
+            //if (i % 2 == 0)
+            //{
+            //    Console.WriteLine("EVEN");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ODD");
+            //}
+
+
+            //string valueOfI = i % 2 == 0 ? "EVEN" : "ODD";
+
+        }
+    }
+}
