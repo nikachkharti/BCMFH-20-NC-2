32079169340edd8901deab7a0856dfea6b25c6c5
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-09-23 21:57:53 +0400

    Strings and functions

---
+++ BCMFH-20-NC-2/Lecture5/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Net.Http.Headers;
+
+namespace Lecture5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int jami = Summary(20, 1);
+            //int jami2 = Summary(77, 2);
+
+            //კალკულატორი რაც წინაზე გავაკეთეთ გადააწყვეთ ფუნქციებზე + - * /
+
+
+            //Console.Write("First Number: ");
+            //int.TryParse(Console.ReadLine(), out int firstNumber);
+
+            //Console.Write("Second Number: ");
+            //int.TryParse(Console.ReadLine(), out int second);
+
+            //Console.Write("+ - * / ");
+            //char.TryParse(Console.ReadLine(), out char operation);
+
+            //switch (operation)
+            //{
+            //    case '+':
+            //        Console.WriteLine(Sum(firstNumber, second));
+            //        break;
+            //    case '-':
+            //        Console.WriteLine(Subtract(firstNumber, second));
+            //        break;
+            //    case '*':
+            //        Console.WriteLine(Multiply(firstNumber, second));
+            //        break;
+            //    case '/':
+            //        Console.WriteLine(Divide(firstNumber, second));
+            //        break;
+            //}
+
+
+            //string name1 = "    Nika Chkahrtishvili  ";
+            //string name2 = "";
+
+            //Console.WriteLine(string.IsNullOrEmpty(name2));
+            //Console.WriteLine(string.IsNullOrWhiteSpace(name2));
+
+
+            //Console.WriteLine(string.Compare(name1, name2));
+            //Console.WriteLine(string.CompareOrdinal(name1, name2));
+            //Console.WriteLine(name1.CompareTo(name2));
+            //Console.WriteLine(name1.Contains("ka", StringComparison.OrdinalIgnoreCase));
+            //Console.WriteLine(name1.Substring(0,3));
+            //Console.WriteLine(name1.EndsWith('a'));
+            //Console.WriteLine(name1.IndexOf('a'));
+            //Console.WriteLine(name1.LastIndexOf('a'));
+            //Console.WriteLine(name1.Insert(name1.Length - 1, "Zadsadsa"));
+            //Console.WriteLine(name1.Replace("ka", "ja"));
+            //string[] splitedName = name1.Split(' ');
+            //Console.WriteLine(name1.ToLower());
+            //Console.WriteLine(name1.ToUpper());
+            //string trimmed = name1.Trim();
+
+
+        }
+
+
+        //static int Sum(int argument1, int argument2)
+        //{
+        //    return argument1 + argument2;
+        //}
+
+        //static int Subtract(int argument1, int argument2)
+        //{
+        //    return argument1 - argument2;
+        //}
+
+        //static int Divide(int argument1, int argument2)
+        //{
+        //    return argument1 / argument2;
+        //}
+
+        //static int Multiply(int argument1, int argument2)
+        //{
+        //    return argument1 * argument2;
+        //}
+
+        //static void SayHello()
+        //{
+        //    Console.WriteLine("Chita");
+        //}
+
+
+        //static void WriteDataInFile(string fullName)
+        //{
+        //    File.WriteAllText(@"../../../TestFile.txt", fullName);
+        //    SayHello();
+        //}
+
+
+
+
+        //დასაბრუნებელი ტიპი  სახელი (int არგუმენტი1, int არგუმენტი2)
+        //
+        //{
+        //  არგუმენტი1 + არგუმენტი2
+        //}
+
+    }
+}
