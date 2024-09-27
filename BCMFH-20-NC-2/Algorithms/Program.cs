8050a85e1d55a923766516a3b4833677023994f8
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-09-27 22:15:18 +0400

    Lecture6

---
+++ BCMFH-20-NC-2/Algorithms/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    //FirstOrDefault
+    //LastOrDefault
+    //FindAll
+    //FindIndex
+    //FindLastIndex
+    //Sum
+    //Reverse
+    //Sort
+    //Max
+    //Min
+    //Any
+    //All
+}
