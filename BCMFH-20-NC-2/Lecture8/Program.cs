748c6a55669e4deed1e320a14fe4ffc961bfe532
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-10-04 21:59:29 +0400

    Base ctor polymorphyism

--- BCMFH-20-NC-2/Lecture8/Program.cs
+++ BCMFH-20-NC-2/Lecture8/Program.cs
@@ -5,22 +5,78 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Age { get; set; }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public Person()
+        {
+        }
+
+        public virtual string Talk()
+        {
+            return $"Hello I am Person {FirstName} {LastName} {Age} years old.";
+        }
     }
 
     public class Employee : Person
     {
         public double Salary { get; set; }
+
+        public Employee(string firstName, string lastName, int age, double salary) : base(firstName, lastName, age)
+        {
+            Salary = salary;
+        }
+
+        public Employee()
+        {
+        }
+
+        public override string Talk()
+        {
+            return $"Hello I am Employee {FirstName} {LastName} {Age} years old, salary {Salary} GEL";
+        }
     }
 
     public class Student : Person
     {
         public double Score { get; set; }
         public Subject Subject { get; set; } //კომპოზიცია
+
+        public Student(string firstName, string lastName, int age) : base(firstName, lastName, age)
+        {
+        }
+
+        public Student()
+        {
+        }
+
+        public override string Talk()
+        {
+            return $"Hello I am Student {FirstName} {LastName} {Age} years old, score {Score} , Subject {Subject.Name}";
+        }
     }
 
     public class Teacher : Employee
     {
         public Subject Subject { get; set; } //კომპოზიცია
+
+        public Teacher(string firstName, string lastName, int age, double salary) : base(firstName, lastName, age, salary)
+        {
+        }
+
+        public Teacher()
+        {
+        }
+
+        public override string Talk()
+        {
+            return $"Hello I am Teacher {FirstName} {LastName} {Age} years old, salary {Salary} GEL , Subject {Subject.Name}";
+        }
     }
 
 
@@ -34,32 +90,49 @@
     {
         static void Main()
         {
-            //1.მემკვიდრეობა
+            //1.მემკვიდრეობა +
 
-            Teacher teacherObj1 = new()
-            {
-                FirstName = "Nika",
-                LastName = "Chkhartishvili",
-                Age = 29,
-                Salary = 500,
-                Subject = new Subject() { Name = "C#" }
-            };
-            Student studObj1 = new()
-            {
-                FirstName = "Saba",
-                LastName = "Ekhvaia",
-                Age = 20,
-                Score = 99,
-                Subject = new Subject() { Name = "C#" }
-            };
+            //Employee employeeObj1 = new()
+            //{
+            //    FirstName = "Giorgi",
+            //    LastName = "Giorgadze",
+            //    Age = 15,
+            //    Salary = 1000
+            //};
+            //Teacher teacherObj1 = new()
+            //{
+            //    FirstName = "Nika",
+            //    LastName = "Chkhartishvili",
+            //    Age = 29,
+            //    Salary = 500,
+            //    Subject = new Subject() { Name = "C#" }
+            //};
+            //Student studObj1 = new()
+            //{
+            //    FirstName = "Saba",
+            //    LastName = "Ekhvaia",
+            //    Age = 20,
+            //    Score = 99,
+            //    Subject = new Subject() { Name = "C#" }
+            //};
+
+            //Console.WriteLine($"EMPLOYEE: {employeeObj1.Talk()}");
+            //Console.WriteLine($"TEACHER: {teacherObj1.Talk()}");
+            //Console.WriteLine($"STUDENT: {studObj1.Talk()}");
 
 
-            HelloFunction(teacherObj1);
+
+            //2.პოლიმორფიზმი +
+
+            //Console.WriteLine(employeeObj1.Talk());
+            //Console.WriteLine(teacherObj1.Talk());
+            //Console.WriteLine(studObj1.Talk());
 
 
 
-            //2.პოლიმორფიზმი
-            //3.ენკაფსულაცია
+            //3.ენკაფსულაცია +
+
+
             //4.აბსტრაქცია
         }
 
