da31ddcdb8312286af8364e07a73a0723e33677b
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-10-15 20:33:59 +0400

    Task

--- BCMFH-20-NC-2/Lecture11/Program.cs
+++ BCMFH-20-NC-2/Lecture11/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //1.ამ ფაილის მიხედვით შექმნათ მანქანის კლასი
+            //2.ამ კლასის მიხედვით უნდა შექმნათ მანქანის ობიექტი
+            //3.ფაილიდან შეძლოთ ერთი ობიექტის შესახებ ინფორმაციის გარდაქმნა და მისი ობიექტად ქცევა
+
+
+            //მოძებნეთ ყველა Mercedes
+            //მოძებნეთ 10 ყველაზე ეკონომიური მანქანა
+
+
+            //მიბრუნებს string ს
+            //string longText = File.ReadAllText("C:\\Users\\User\\Desktop\\IT STEP\\BCMFH-20-NC-2\\BCMFH-20-NC-2\\Lecture11\\vehicles.csv");
+
+            //მიბრუნებს string ის მასივს 
+            //string[] longArray = File.ReadAllLines("C:\\Users\\User\\Desktop\\IT STEP\\BCMFH-20-NC-2\\BCMFH-20-NC-2\\Lecture11\\vehicles.csv");
+
         }
     }
 }
