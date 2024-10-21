545bdac9a7c65204ec14059925529a2e8e69493e
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-10-21 21:32:12 +0400

    .

--- BCMFH-20-NC-2/Lecture13/Program.cs
+++ BCMFH-20-NC-2/Lecture13/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //დაწერეეთ ლოგერი ლოგერი (ფუნქცია)
+            //უნდა იღებდეს რაღაც ტექსტს და დელეგატს რომელზეც შემეძლება
+            //ფუნქციის მიცემა თუ სად მსურს გადაცემული ტექსტის დალოგვა, ფაილში თუ კონსოლში
+
+
         }
+
+        public static void Logger(string message,//დელეგატი)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
