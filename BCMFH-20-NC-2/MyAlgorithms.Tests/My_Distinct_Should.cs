f0b5ec2ab3ecdbab2bd6ce010c923716eb1102d0
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-11-01 22:18:11 +0400

    Value and reference types

---
+++ BCMFH-20-NC-2/MyAlgorithms.Tests/My_Distinct_Should.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MyAlgorithms.Tests
+{
+    class StudentEquilityComparer : IEqualityComparer<Student>
+    {
+        public bool Equals(Student x, Student y) => x.Name.Trim().ToLower() == y.Name.Trim().ToLower() && x.Age == y.Age;
+        public int GetHashCode([DisallowNull] Student obj) => obj.Name.Length;
+    }
+
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override bool Equals(object obj) => new StudentEquilityComparer().Equals(this, obj as Student);
+        public override int GetHashCode() => new StudentEquilityComparer().GetHashCode(this);
+    }
+
+    public class My_Distinct_Should
+    {
+        [Fact]
+        public void Find_Unique_Value_Elements()
+        {
+            //Arrange
+            List<int> intList = new() { 1, 10, 10, 2 };
+
+            //Act
+            var expected = new HashSet<int>() { 1, 10, 2 };
+            var actual = intList.MyDistinct();
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void Find_Unique_Reference_Elements()
+        {
+            //Arrange
+            List<Student> studentList = new()
+            {
+                new Student() {Name = "Ketevan",Age = 20},
+                new Student() {Name = "Ilia",Age = 22},
+                new Student() {Name = "Ilia",Age = 22},
+                new Student() {Name = "Ilia",Age = 22},
+                new Student() {Name = "Tengiz",Age = 31},
+                new Student() {Name = "Tengiz",Age = 31},
+                new Student() {Name = "Petre",Age = 12}
+            };
+
+            //Act
+            var expected = new HashSet<Student>()
+            {
+                new Student() {Name = "Ketevan",Age = 20},
+                new Student() {Name = "Ilia",Age = 22},
+                new Student() {Name = "Tengiz",Age = 31},
+                new Student() {Name = "Petre",Age = 12}
+            };
+
+            var actual = studentList.MyDistinct(new StudentEquilityComparer());
+
+            //Assert
+            Assert.Equal(expected, actual, new StudentEquilityComparer());
+        }
+
+    }
+}
---
+++ BCMFH-20-NC-2/MyAlgorithms.Tests/Value_Reference_Tests.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Channels;
+
+namespace MyAlgorithms.Tests
+{
+    public class MoneyReference
+    {
+        public string Currency { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+    public struct MoneyValue
+    {
+        public string Currency { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+
+    public class Value_Reference_Tests
+    {
+        [Fact]
+        public void Equal()
+        {
+            MoneyValue mv1 = new() { Amount = 100, Currency = "GEL" };
+            MoneyValue mv2 = new() { Amount = 100, Currency = "USD" };
+
+            MoneyReference mr1 = new() { Amount = 100, Currency = "GEL" };
+            MoneyReference mr2 = /*new() { Amount = 100, Currency = "GEL" };*/ mr1;
+
+            Assert.Equal(mr1, mr2);
+        }
+    }
+}
