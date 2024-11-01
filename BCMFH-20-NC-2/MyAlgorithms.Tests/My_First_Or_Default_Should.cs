17f9aa00457616450acf963194af2349b5f9c3f9
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-11-01 21:03:06 +0400

    Unit tests

---
+++ BCMFH-20-NC-2/MyAlgorithms.Tests/My_First_Or_Default_Should.cs
@@ -0,0 +1,34 @@
+﻿namespace MyAlgorithms.Tests
+{
+    public class My_First_Or_Default_Should
+    {
+        [Fact]
+        public void Find_First_Element_In_IEnumerable()
+        {
+            //Arrange
+            List<int> intList = new() { -1, 2, 3, -4 };
+
+            //Act
+            var expected = -1;
+            var actual = intList.MyFirstOrDefault(x => x < 0);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Return_Default_Value()
+        {
+            //Arrange
+            List<int> intList = new() { -1, 2, 3, -4 };
+
+            //Act
+            var expected = 0;
+            var actual = intList.MyFirstOrDefault(x => x > 100);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
