14601199eb7705b7cae2582eafc164f2a3a02692
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-11-08 21:08:41 +0400

    Json task

---
+++ BCMFH-20-NC-2/MiniBank.Models/Account.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniBank.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Iban { get; set; }
+        public string Currency { get; set; }
+        public decimal Balance { get; set; }
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ BCMFH-20-NC-2/MiniBank.Repository/AccountJsonRepository.cs
@@ -0,0 +1,56 @@
+﻿using MiniBank.Models;
+
+namespace MiniBank.Repository
+{
+    public class AccountJsonRepository
+    {
+        private readonly string _filePath;
+        private readonly List<Account> _accounts;
+
+        public AccountJsonRepository(string filePath)
+        {
+            _filePath = filePath;
+            _accounts = LoadData();
+        }
+
+        public List<Account> GetAccounts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Account> GetAccounts(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Account GetAccount(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SaveData()
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<Account> LoadData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
