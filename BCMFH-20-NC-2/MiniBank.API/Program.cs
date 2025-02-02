501d59ab71e5f0df1d67632ab80cc4fd00886a2b
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-02-02 13:57:27 +0400

    API

---
+++ BCMFH-20-NC-2/MiniBank.API/Program.cs
@@ -0,0 +1,26 @@
+
+namespace MiniBank.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
